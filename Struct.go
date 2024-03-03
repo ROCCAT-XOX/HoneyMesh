796ecf8d1b0634c99dc0abbf57acaef295f7c4f7
Yannick Stumpf <yannick.stumpf@smart3ch.de>
2024-03-03 23:16:47 +0100

    fixed Struct

--- Struct.go
+++ Struct.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"context"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
-	"time"
-)
-
 type SensorData struct {
 	Date  string     `bson:"date"`
 	Time  string     `bson:"time"`
@@ -24,25 +16,4 @@ type NodeData struct {
 	WifiStrength float64 `bson:"wifistrength"`
 	Date         string  `bson:"date"`
 	Time         string  `bson:"time"`
-}
-
-// Funktion zum Herstellen einer Verbindung mit MongoDB
-func connectToMongo() (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil {
-		return nil, err
-	}
-
-	// Überprüfen der Verbindung
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Println("Erfolgreich mit MongoDB verbunden.")
-	return client, nil
 }
