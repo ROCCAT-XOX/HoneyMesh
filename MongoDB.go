8def7178c26a7ac74378f42e35874396ae265003
Yannick Stumpf <yannick.stumpf@smart3ch.de>
2024-03-03 23:16:35 +0100

    connect to DB

--- MongoDB.go
+++ MongoDB.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+// Funktion zum Herstellen einer Verbindung mit MongoDB
+func connectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Überprüfen der Verbindung
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Erfolgreich mit MongoDB verbunden.")
+	return client, nil
+}
